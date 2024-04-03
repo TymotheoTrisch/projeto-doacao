e6321f786a163d00bddd7920ea20142a72520b38
TymotheoTrisch <tymotheo.dev@gmail.com>
2024-04-02 22:48:35 -0300

    Commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', function() {
         if (this.checked) {
-            this.previousElementSibling.style.textDecoration = "line-through"; // Exemplo: sublinhando o label
+            this.previousElementSibling.style.textDecoration = "line-through";
         } else {
-            this.previousElementSibling.style.textDecoration = "none"; // Exemplo: removendo o sublinhado do label
+            this.previousElementSibling.style.textDecoration = "none";
         }
     });
 });
